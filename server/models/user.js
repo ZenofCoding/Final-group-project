8044f00c3f07e4827eb8eefa7b0e1cca82cd24ec
Mike Magnani <mikemagnani5@gmail.com>
2016-10-04 18:16:23 -0400

    added controller and models files

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = mongoose.model('User', User);
