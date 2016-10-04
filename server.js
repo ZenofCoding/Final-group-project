5558f6cd8d74f4f4440bc4d48ee8698b33b69cfc
Mike Magnani <mikemagnani5@gmail.com>
2016-10-04 16:23:22 -0400

    created basic file structure

---
+++ server.js
@@ -0,0 +1,19 @@
+var express           = require('express'),
+    app               = express();
+   // bodyParser        = require('body-parser'),
+   // mongoose          = require('mongoose'),
+  
+
+//app.use(bodyParser());
+
+
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/client/views/index.html');
+});
+
+app.use('/js', express.static(__dirname + '/client/js'));
+
+app.listen(3000, function() {
+  console.log('I\'m Listening...');
+})
