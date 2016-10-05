b77ce5c1b3e1c90fa3a0f9372a5dc85b8c3cf100
Angelo Bovino <angelo.bovino@outlook.com>
2016-10-05 00:01:09 -0400

    commit

--- server.js
+++ server.js
@@ -1,12 +1,67 @@
-var express           = require('express'),
-    app               = express();
-   // bodyParser        = require('body-parser'),
-   // mongoose          = require('mongoose'),
-  
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var FitbitStrategy = require('./client/oauth').FitbitOAuth2Strategy;
+var passport = require('passport');
 
-//app.use(bodyParser());
+//var mongoose = require('mongoose'),
+var app = express();
 
+app.use(cookieParser());
+app.use(bodyParser());
 
+app.use(session({ secret: 'keyboard cat' }));
+
+app.use(passport.initialize());
+app.use(passport.session({
+  resave: false,
+  saveUninitialized: true
+}));
+
+const CLIENT_ID = '22828X';
+const CLIENT_SECRET = 'd24dfc6f7bb64f374459e04747b67cf8';
+
+app.use(passport.initialize());
+
+var fitbitStrategy = new FitbitStrategy({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  scope: ['activity','heartrate','location','profile'],
+  callbackURL: "http://localhost:3000/auth/fitbit/callback"
+}, function(accessToken, refreshToken, profile, done) {
+  // TODO: save accessToken here for later use
+
+  done(null, {
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+    profile: profile
+  });
+
+});
+
+passport.use(fitbitStrategy);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var fitbitAuthenticate = passport.authenticate('fitbit', {
+  successRedirect: '/auth/fitbit/success',
+  failureRedirect: '/auth/fitbit/failure'
+});
+
+app.get('/auth/fitbit', fitbitAuthenticate);
+app.get('/auth/fitbit/callback', fitbitAuthenticate);
+
+app.get('/auth/fitbit/success', function(req, res, next) {
+  console.log("accessToken = " + req.user.accessToken);
+  res.send(req.user);
+});
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/client/views/index.html');
